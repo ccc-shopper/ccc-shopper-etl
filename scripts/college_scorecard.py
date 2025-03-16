7f3e154ea475c670841754e7a6530a9ee07d4fb6
Cody Vig <85211320+CodyVig@users.noreply.github.com>
2025-03-16 15:40:26 -0700

    Scorecard and Labor Market Data (#9)
    * Updated script functionality.
    
    * Separated `merge.ipynb` into `college_level_data.ipynb` and `labor_market.ipynb`
    
    * Updated figures and HTMLs files.

--- scripts/college_scorecard.py
+++ scripts/college_scorecard.py
@@ -78,3 +78,89 @@ def get_latest_student_scorecard_data_by_state(
         student = pd.concat([student, df_temp])
 
     return student
+
+
+def get_scorecard_by_college(
+        college_name: str,
+        college_city: str | None = None,
+        college_state: str | None = None
+    ) -> pd.DataFrame:
+    """
+    Get the latest information for a specified college from the College Scorecard API
+
+    :param college_name: The name of the college
+    :param college_city: The city where the college is located
+    :param college_state: The state where the college is located
+    :return: A DataFrame containing the latest information for the college
+    """
+
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools"
+
+    # There are hundreds of fields available in the College Scorecard API.
+    # These are the ones that are the most readily useful for our purposes.
+    fields = [
+        "school.name",
+        "latest.student.size",
+        "latest.student.enrollment.undergrad_12_month",
+        "latest.student.demographics.over_23_at_entry",
+        "latest.student.demographics.first_generation",
+        "latest.student.demographics.median_hh_income",
+        "latest.student.demographics.student_faculty_ratio",
+        "latest.student.FAFSA_applications",
+    ]
+
+    params = {
+        "api_key": _get_api_key(),
+        "school.name": college_name,
+        "fields": ",".join(fields),
+    }
+
+    if college_city:
+        params["school.city"] = college_city
+
+    if college_state:
+        params["school.state"] = college_state
+
+    response = requests.get(url, params=params)
+    data = response.json()["results"]
+
+    # Adding missing fields to the data.
+    for college in data:
+        for field in fields:
+            if field not in college:
+                college[field] = 0
+
+    # Replacing periods with underscores in the keys.
+    data = [
+        {
+            k.replace(".", "_"): v for k, v in college.items()
+        } for college in data
+    ]
+
+    data = pd.DataFrame(data)
+    # Reorder the columns so that `school_name` is the first column.
+    columns = ["school_name"] + [col for col in data.columns if col != "school_name"]
+    data = data[columns]
+    data[
+        [
+            "latest_student_size",
+            "latest_student_enrollment_undergrad_12_month",
+            "latest_student_demographics_over_23_at_entry",
+            "latest_student_demographics_first_generation",
+            "latest_student_demographics_median_hh_income",
+            "latest_student_demographics_student_faculty_ratio",
+            "latest_student_FAFSA_applications",
+        ]
+    ] = data[
+        [
+            "latest_student_size",
+            "latest_student_enrollment_undergrad_12_month",
+            "latest_student_demographics_over_23_at_entry",
+            "latest_student_demographics_first_generation",
+            "latest_student_demographics_median_hh_income",
+            "latest_student_demographics_student_faculty_ratio",
+            "latest_student_FAFSA_applications",
+        ]
+    ].apply(pd.to_numeric)
+
+    return data
